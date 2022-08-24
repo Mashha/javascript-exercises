2effe2b82cea93c0270d9372e16464e3a293744c
Masa Orel <orel.masha89@gmail.com>
2022-08-24 15:44:19 +0200

    04 done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function (array, ...args) {
+  //splice makes you remove an element
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < args.length; j++) {
+      if (array[i] === args[j]) {
+        array.splice(i, 1)
+        i--
+      }
+    }
+  }
+  return array
+}
 
 // Do not edit below this line
-module.exports = removeFromArray;
+module.exports = removeFromArray
