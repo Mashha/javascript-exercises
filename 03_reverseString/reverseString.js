8e9a0303e967e89544570f9b0783db5e5b3864b9
Masa Orel <orel.masha89@gmail.com>
2022-08-24 14:12:43 +0200

    03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  //first we need to add it to array
+  // let addToArray = string.split("");
+  // // reverse the array
+  // let reverseArray = addToArray.reverse()
+  // //join back
+  // let joinTheString = reverseArray.join("")
 
-};
+  // shorter
+  return string.split('').reverse().join('')
+}
 
 // Do not edit below this line
-module.exports = reverseString;
+module.exports = reverseString
